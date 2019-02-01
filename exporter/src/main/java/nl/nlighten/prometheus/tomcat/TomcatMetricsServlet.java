92a7f94b56a6ce33d802dad88df2399fdf406669
richard.swart@nlighten.nl <richard.swart@nlighten.nl>
2019-02-01 20:49:52 +0100

    do not register when already registered. Fixes #15

--- exporter/src/main/java/nl/nlighten/prometheus/tomcat/TomcatMetricsServlet.java
+++ exporter/src/main/java/nl/nlighten/prometheus/tomcat/TomcatMetricsServlet.java
@@ -4,8 +4,14 @@ package nl.nlighten.prometheus.tomcat;
 import javax.servlet.ServletConfig;
 import javax.servlet.annotation.WebServlet;
 
+import io.prometheus.client.Collector;
+import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.exporter.MetricsServlet;
 import io.prometheus.client.hotspot.DefaultExports;
+
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.HashSet;
 
 
 @WebServlet("/")
@@ -13,13 +19,20 @@ public class TomcatMetricsServlet extends MetricsServlet {
 
     @Override
     public void init(ServletConfig config) {
-        DefaultExports.initialize();
-        new TomcatGenericExports(false).register();
-        if (TomcatJdbcPoolExports.isTomcatJdbcUsed()) {
-            new TomcatJdbcPoolExports().register();
-        } else {
-            new TomcatDbcp2PoolExports().register();
+        if (!initialized()) {
+            DefaultExports.initialize();
+            new TomcatGenericExports(false).register();
+            if (TomcatJdbcPoolExports.isTomcatJdbcUsed()) {
+                new TomcatJdbcPoolExports().register();
+            } else {
+                new TomcatDbcp2PoolExports().register();
+            }
         }
+    }
+
+    private boolean initialized() {
+        Enumeration<Collector.MetricFamilySamples> samples = CollectorRegistry.defaultRegistry.filteredMetricFamilySamples(new HashSet<gitString>(Arrays.asList("tomcat_info")));
+        return samples.hasMoreElements();
     }
 }
 
